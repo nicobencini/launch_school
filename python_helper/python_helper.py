3a866fa002e1ded661f9a591870d3a3a2c04422a
Nicolo Bencini <nicbencini@gmail.com>
2024-07-27 22:04:21 +0100

    Added more studying notes

--- python_helper/python_helper.py
+++ python_helper/python_helper.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 PATH = "/home/nicbencini/launch_school/python_helper/python_helper_db.json"
 
@@ -12,6 +13,24 @@ def helper_reader(keyword):
         description = f"==> '{keyword}' not found!!"
 
     return (description)
+
+def helper_search(keyword):
+    with open(PATH, 'r') as f:
+        info_dictionary = json.load(f)
+
+    keys = info_dictionary.keys()
+    
+    count = 0
+
+    for key in keys:
+        if keyword in key:
+            print(f'\n{key}:\n' + '-'*20)
+            print(info_dictionary[key])
+            print('-'*20 + '\n')
+            count += 1
+    
+    if count == 0:
+        print ('==> No iformation found')
 
 def helper_writer(keyword, description):
     
@@ -41,8 +60,9 @@ print('*' *100 )
 print('*' *100 )
 print('Welcome to Python Helper!\n')
 print("Input 'r- + keyword' to retrieve the informaiton on that keyword.")
-print("Input 'w- + keyword' to write a new entry.")
+print("Input 's- + keyword' to search the database for all entries containing the keyword and print results.")
 print("Input 'keys' to get a list of the current keywords.")
+print("Input 'study' to go into study mode.")
 print("Input 'q' to exit.")
 print('*' *100 + '\n')
 
@@ -56,16 +76,28 @@ while True:
         print(f'\n{keyword}:\n' + '-'*20)
         print(helper_reader(keyword))
         print('-'*20 + '\n')
-    elif(user_input[0:2] == 'w-'):
-        print('==> Functionality removed. Edit JSON file directly.')
-        #keyword = user_input[2::]
-        #description = input('==> Please enter information to store:')
-        #helper_writer(keyword, description)
-        #print(f"==> '{keyword}' entry has now been stored.")
-
+    elif(user_input[0:2] == 's-'):
+        keyword = user_input[2::]
+        helper_search(keyword)
     elif(user_input == 'q'):
         print('==> Goodbye!')
         break
+    elif(user_input == 'study'):
+        with open(PATH, 'r') as f:
+            info_dictionary = json.load(f)
+
+        keys = list(info_dictionary.keys())
+
+        while True:
+            key = random.choice(keys)
+            print('-'*20)
+            print(key)
+            user_input = input("==>Press enter for answer or 'q' to quit.\n\n")
+            if user_input == 'q':
+                break
+            print (info_dictionary[key])
+            print('-'*20)
+
     else:
         print('==> Invalid input. Please re-entre.')
 
